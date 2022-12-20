e5806c36b5b45eac79920d2811d9697a6ea16141
Wlad Neto <wladimirteixeira@yahoo.com.br>
2022-12-19 21:48:29 -0300

    fix: remove exposed ApiKey

--- maps/dist/index.js
+++ maps/dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37319" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34089" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
